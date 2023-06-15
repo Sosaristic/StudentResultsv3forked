0248970e7333e37a61ee74b6dd02415e9211f853
Sosaristic <sundayomena9@gmail.com>
2023-06-16 00:57:29 +0100

    increased the with of authentication pages for mobile

--- src/components/UserAuthenticationLayout.jsx
+++ src/components/UserAuthenticationLayout.jsx
@@ -23,11 +23,11 @@ export function UserAuthenticationLayout() {
       </nav>
       <main className="w-full p-4 h-full flex flex-col items-center text-zinc-700 relative overflow-x-hidden">
         <h2 className="text-xl font-medium justify-self-start">FUTO Students Result Portal</h2>
-        <div className="w-[90%] md:w-[60%] lg:w-[50%] bg-white rounded-md shadow-xl px-10 py-5 flex flex-col gap-4">
+        <div className="w-[100%] md:w-[60%] lg:w-[50%] bg-white rounded-md shadow-xl mt-2 px-10 py-5 flex flex-col gap-4">
           <div className="p-5 bg-dark-green rounded-full mx-auto w-max h-max ">
             <AcademicCapIcon className="h-10 text-white" />
           </div>
-          <p className="mx-auto my-2 text-lg font-[500]">{headerText(pathname)}</p>
+          <p className="mx-auto my-2 text-lg font-[500] mt-4">{headerText(pathname)}</p>
           <Outlet />
           <p className="mx-auto text-sm text-zinc-500">Powered by Ulpha Deep Labs</p>
         </div>
