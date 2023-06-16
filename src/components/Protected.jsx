73dfb64370ff4f03960f2f6d5127c12b6d1867f9
Sunday Omena <98474375+Sosaristic@users.noreply.github.com>
2023-06-16 16:36:28 +0100

    Remove desktop side nav from mobile

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -9,11 +9,11 @@ export function Protected() {
   }
   return (
     <div className="bg-background min-h-screen relative flex ">
-      <div className="w-2/12 bg-dark-green rounded-tr-2xl rounded-br-2xl px-4">
+      <div className="w-2/12 hidden lg:block bg-dark-green rounded-tr-2xl rounded-br-2xl px-4">
         <DesktopSideNav />
       </div>
 
-      <div className="w-10/12">
+      <div className="w-10/12 lg:full">
         <Outlet />
       </div>
     </div>
