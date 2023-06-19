ab886cc562cb54c5c67f730bd376f3f7ac2b7285
Sosaristic <sundayomena9@gmail.com>
2023-06-19 01:37:41 +0100

    modified the height of the display cards

--- src/components/dashboard/dashboard/PerformanceCards.jsx
+++ src/components/dashboard/dashboard/PerformanceCards.jsx
@@ -8,7 +8,7 @@ export default function PerformanceCard() {
   const {userData} = useAppContext()
   return (
     <div className="flex flex-col px-4 gap-2 md:flex-row md:items-center font-inter justify-between">
-      <div className="flex justify-around border min-h-[4rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
+      <div className="flex justify-around border h-[8rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
         <div>
           <img src={Graduation} alt="" height={100} width={100} />
         </div>
@@ -18,7 +18,7 @@ export default function PerformanceCard() {
         </div>
       </div>
 
-      <div className="flex border min-h-[4rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
+      <div className="flex border min-h-[8rem] h-[8rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
         <div>
           <img src={CarryOver} alt="" height={100} width={100} />
         </div>
@@ -28,7 +28,7 @@ export default function PerformanceCard() {
         </div>
       </div>
 
-      <div className="flex border min-h-[4rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
+      <div className="flex border min-h-[4rem] h-[8rem] max-h-[8rem] bg-dark-green p-4 text-grey-white shadow-dashboard-card rounded-md">
         <div>
           <img src={Time} alt="" height={100} width={100} />
         </div>
